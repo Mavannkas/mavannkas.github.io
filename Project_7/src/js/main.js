3f5c57c7265d355b395fdad9c032354777138331
Wiktor <wiktor.ludwiniak@gmail.com>
2021-06-08 19:58:23 +0200

    proj 7

--- Project_7/src/js/main.js
+++ Project_7/src/js/main.js
@@ -1,9 +1,11 @@
 const hamburgerButton = document.querySelector('.hamburger');
 const navClose = document.querySelector('.nav__list-close');
 const mobileMenu = document.querySelector('.nav__list--mobile');
+const actualDate = document.querySelector('#actual-date');
 
 const toggleMobileMenu = () => {
     mobileMenu.classList.toggle('show');
 }
 
 [hamburgerButton, navClose].forEach( el => el.addEventListener('click', toggleMobileMenu));
+actualDate.innerText = (new Date()).getFullYear();
